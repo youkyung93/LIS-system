f8a27c8ee61f31ee63891c648d75aae73cd7184e
dhdmsgp6263 <dhdmsgp6263@nate.com>
2014-12-05 02:15:06 +0900

    display
    display modified

---
+++ LIS-system/display.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void displaybook();
+
+void displaybook(){
+	
+	printf("현재 대출중인 도서 목록\n");
+	
+	//DB 이용
+	/*
+	while(DB.Renting_student == ID){
+		printf(DB.bookname);
+		printf(DB.author);
+		}
+
+	menu = func_std;
+	showmenu();
+	*/
+
+}
