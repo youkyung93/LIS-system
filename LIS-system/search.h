ad60fbaaf1977a0c52e2efaffa920de5887067c9
dhdmsgp6263 <dhdmsgp6263@nate.com>
2014-12-05 02:17:31 +0900

    search.h modified
    기본 틀 생성

---
+++ LIS-system/search.h
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void searchbook();
+
+
+void searchbook(){
+	char keyword[100];
+
+	int booknumber;
+	int i=1;
+	
+
+	printf("검색할 도서의 이름을 입력하세요 : ");
+	scanf("%c",keyword);
+
+	//DB 이용
+	/*
+	while('%'keyword'%' == DB.bookname){
+	//	printf("%d . %c",i,DB.bookname)
+	}*/
+	printf("원하는 도서번호를 선택하세요 : ");
+	scanf("%d",booknumber);
+
+	//DB이용
+	/*printf(DB.title); 이런형식
+	*/
+
+	//menu = func_std;
+	//showmenu();
+}
