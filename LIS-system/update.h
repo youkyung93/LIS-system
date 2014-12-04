d5761738632873fe738c63152bab2d72fdffcd2c
fbdkdud <fbdkdud93@hanmail.net>
2014-12-04 18:00:53 +0900

    delete.h update.h are modified

---
+++ LIS-system/update.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void updateBook();
+
+void updateBook(){
+	char searchbook[100] = {0,};
+	char *searchbook;
+
+	int booknumber;
+	
+	printf("원하는 도서의 이름을 입력하세요.");
+	scanf("%c", searchbook);
+
+	//여기에 검색한 단어를 포함하는 책 목록을 DB에서 불러와서 앞에 번호를 붙이는 함수 쓰기
+
+	printf("원하는 도서의 번호를 입력하세요.");
+	scanf("%d", &booknumber);
+
+	//DB에서 해당 책 삭제하는 기능 구현
+
+	printf("도서가 삭제되었습니다.");
+}
