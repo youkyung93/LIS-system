96fcb8a13208aeca6a81ff5b16014bf6ea762955
fbdkdud <fbdkdud93@hanmail.net>
2014-12-05 15:48:19 +0900

    update.h is changed

--- LIS-system/update.h
+++ LIS-system/update.h
@@ -1,22 +1,36 @@
 #include <stdio.h>
 
-void updateBook();
-
 void updateBook(){
-	char searchbook[100] = {0,};
+	char searchbook[100]= {0,};
 	char *searchbook;
+	char choice = 'Y';
 
-	int booknumber;
-	
-	printf("원하는 도서의 이름을 입력하세요.");
+	int booknumber = 0;
+	int bookinfonum = 0;
+
+	printf("수정하고 싶은 책의 이름을 입력하세요 : ");
 	scanf("%c", searchbook);
 
-	//여기에 검색한 단어를 포함하는 책 목록을 DB에서 불러와서 앞에 번호를 붙이는 함수 쓰기
+	//DB에서 책 목록을 꺼내오는 함수. 꺼내올 때 책 목록 앞에 번호가 붙여진다. 이때 책의 이름과 저자의 이름만 보여진다.
 
-	printf("원하는 도서의 번호를 입력하세요.");
+	printf("원하는 도서의 번호를 입력하세요 : ");
 	scanf("%d", &booknumber);
+	
+	//여기에서 해당 책의 정보가 나열된다. 이때 각 정보들 앞에 번호가 붙어있다.
+	printf("어떤 정보를 수정하시겠습니까? ");
+	scanf("%d", &bookinfonum);
+	
+	if(choice =='Y'){
+		puts("다른 정보도 수정하시겠습니까? (Y/N)");
+		fflush(stdin);
+		choice =  getchar();
 
-	//DB에서 해당 책 삭제하는 기능 구현
-
-	printf("도서가 삭제되었습니다.");
+		//if()  위에 나타난 번호를 벗어나지 않는다면
+			printf("책의 정보가 성공적으로 수정 되었습니다.");
+		//else 위에 나타난 번호를 벗어나는 숫자를 입력한다면
+			printf("원하는 책의 번호를 다시 입력해주세요 : ");
+			scanf("%d", &booknumber);
+	}
+	else
+		menu(); //몇번menu일지는 모르겠지만 librarian의 기능을 고를 수 있는 menu로 이동하도록 한다. 
 }
