c92ce1e87c52b6e378373802df15ec8dd8c9ad4c
youkyung93 <sangandhooni@naver.com>
2014-12-08 00:51:19 +0900

    account.h 기본 틀 잡음
    기본틀 잡았음

--- LIS-system/account.h
+++ LIS-system/account.h
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include "menu.h"
 
 void creatAccount() {
 
+	int std_id;
+	char std_password[10];
+	char std_name[50];
+	char std_department[50];
 
+	printf("ID: ");
+	scanf("%d", &std_id);
+
+	printf("PASSWORD: ");
+	scanf("%s", &std_password);
+
+	printf("NAME: ");
+	scanf("%s", &std_name);
+
+	printf("DEPARTMENT: ");
+	scanf("%s", &std_department);
 }
